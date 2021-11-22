eae7c5005ce718e182bcf168112358128b61368d
FthTopcu <fthtpu@gmail.com>
2021-11-22 18:36:17 +0300

    Fixxing

--- ArrayListUyg/Program.cs
+++ ArrayListUyg/Program.cs
@@ -13,12 +13,10 @@ namespace ArrayListUyg
         static void Main(string[] args)
         {
             ArrayList arrayList = new ArrayList();
-            
-            
             do
             {
                 string o1;
-                Console.WriteLine("Menu\n");
+                Console.WriteLine("\nMenu\n");
                 Console.WriteLine("1-Değer Ekle");
                 Console.WriteLine("2-Değer Listele");
                 Console.WriteLine("3-Değer Ara");
@@ -30,11 +28,14 @@ namespace ArrayListUyg
                 switch (secim)
                 {
                     case 1:
+
                         Console.Clear();
                         Console.Write("Eklemek istediğiniz değeri giriniz :");
                         arrayList.Add(Console.ReadLine());
                         break;
+
                     case 2:
+
                         Console.Clear();
                         Console.WriteLine("Değerler :");
                         for (int i = 0; i < arrayList.Count; i++)
@@ -42,7 +43,9 @@ namespace ArrayListUyg
                             Console.WriteLine((i+1)+".değer :"+arrayList[i]);
                         }
                         break;
+
                     case 3:
+
                         Console.Clear();
                         Console.Write("Aramak istediğiniz değeri giriniz :");
                         o1 = Console.ReadLine();
@@ -55,7 +58,9 @@ namespace ArrayListUyg
                             Console.WriteLine("Aradığınız değer listede yok.");
                         }
                         break;
+
                     case 4:
+
                         Console.Clear();
                         for (int i = 0; i <arrayList.Count ; i++)
                         {
@@ -67,7 +72,9 @@ namespace ArrayListUyg
                         arrayList[index - 1] = Console.ReadLine();
                         Console.WriteLine("Değiştirildi.");
                         break;
+
                     case 5:
+
                         Console.Clear();
                         for (int i = 0; i < arrayList.Count; i++)
                         {
@@ -77,11 +84,14 @@ namespace ArrayListUyg
                         int index2 = Convert.ToInt32(Console.ReadLine());
                         arrayList.RemoveAt(index2 - 1);
                         break;
+
                     case 6:
+
                         Console.WriteLine("iyi günler");
                         Thread.Sleep(2000);
                         Environment.Exit(0);
                         break;
+
                     default:
                         Console.WriteLine("Girdiğiniz değeri kontrol ediniz");
                         break;
